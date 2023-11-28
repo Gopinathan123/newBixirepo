cd79b2e28a769b1f559eaaa59aadab5e25c9d7ed
LENOVO <LENOVO@DESKTOP-1VN6NSF>
2023-11-28 14:59:32 +0530

    New Project

---
+++ src/test/java/newPackage/GopiPractice.java
@@ -0,0 +1,13 @@
+package newPackage;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class GopiPractice {
+	@Test
+	public void practice()
+	{
+		Reporter.log("Welcome to gitHub",true);
+	}
+
+}
