0f1f8df66ea3bdfa05ee15846158ff7407c043aa
LENOVO <LENOVO@DESKTOP-1VN6NSF>
2023-11-28 15:34:45 +0530

    updated

---
+++ src/test/java/newPackage/GopiPractice2.java
@@ -0,0 +1,11 @@
+package newPackage;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class GopiPractice2 {
+@Test
+public void main() {
+	Reporter.log("Clone Created successfully");
+}
+}
